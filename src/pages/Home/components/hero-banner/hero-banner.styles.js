c1f36c5666b5c609b660b0891bcaf0707bf9c915
Adetayo Chukwudi <adetayo.ca@gmail.com>
2023-10-10 18:44:10 -0400

    minor changes made

--- src/pages/Home/components/hero-banner/hero-banner.styles.js
+++ src/pages/Home/components/hero-banner/hero-banner.styles.js
@@ -2,7 +2,7 @@ export const styles = {
   image: {
     marginTop: "-10%",
     width: "100%",
-    height: "100%",
+    height: "100vh",
     maxHeight: "50em",
     backgroundColor: "green",
     filter: "brightness(60%)",
